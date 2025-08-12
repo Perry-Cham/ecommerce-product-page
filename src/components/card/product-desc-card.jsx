cadc035164e78f8b1dce8cc38c651c997e154dfa
Perry-Cham <pchamileke@gmail.com>
2025-08-13 00:04:40 +0200

    rearranged card components

--- src/components/card/product-desc-card.jsx
+++ src/components/card/product-desc-card.jsx
@@ -5,19 +5,23 @@ function Product_Desc ({cart}){
         <h2>SNEAKER COMPANY</h2>
         <h3>Fall Limited Sneakers</h3>
         <p>These low-profile sneakers are your perfect casual wear
-        companion.
-        Featuring a durable rubber outer sole, they’ll withstand everything
+        companion. Featuring a durable rubber outer sole, they’ll withstand everything
         the weather can offer.</p>
-        <div>   <p className="price">$125.00</p>
-        <p className="discount">50%</p></div>
+        <div>   
+        <p className="price">$125.00</p>
+        <p className="discount">50%</p>
+        </div>
         <p className="price old">$250.00</p>
 
         <div className="cart-btn-wrapper">
+
         <div className="cart-handler">
+        <div>
         <img className="minus" src="/images/icon-minus.svg"/>
         <p className="number">cart.length</p>
         <img className="plus" src="/images/icon-plus.svg"/>
         </div>
+
         <button className="cart-add-btn">
         <img className="" src="/images/icon-cart.svg">
         Add to cart
@@ -25,7 +29,6 @@ function Product_Desc ({cart}){
         </div>
 
         </div>
-
     })
 }
 export default Product_Desc;
