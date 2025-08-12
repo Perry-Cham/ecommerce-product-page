f830d8f6357b74863017183d9295a5d9ab1c61f8
Perry-Cham <pchamileke@gmail.com>
2025-08-13 00:08:58 +0200

    Fixed errors in card component

--- src/components/card/product-desc-card.jsx
+++ src/components/card/product-desc-card.jsx
@@ -1,34 +1,33 @@
-
-function Product_Desc ({cart}){
-    return({
-        <div>
-        <h2>SNEAKER COMPANY</h2>
-        <h3>Fall Limited Sneakers</h3>
-        <p>These low-profile sneakers are your perfect casual wear
-        companion. Featuring a durable rubber outer sole, they’ll withstand everything
-        the weather can offer.</p>
-        <div>   
+function Product_Desc({ cart }) {
+  return (
+    <div>
+      <h2>SNEAKER COMPANY</h2>
+      <h3>Fall Limited Sneakers</h3>
+      <p>
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they’ll withstand everything the
+        weather can offer.
+      </p>
+      <div>
         <p className="price">$125.00</p>
         <p className="discount">50%</p>
-        </div>
-        <p className="price old">$250.00</p>
+      </div>
+      <p className="price old">$250.00</p>
 
-        <div className="cart-btn-wrapper">
-
+      <div className="cart-btn-wrapper">
         <div className="cart-handler">
-        <div>
-        <img className="minus" src="/images/icon-minus.svg"/>
-        <p className="number">cart.length</p>
-        <img className="plus" src="/images/icon-plus.svg"/>
-        </div>
+          <div>
+            <img className="minus" src="/images/icon-minus.svg" />
+            <p className="number">cart.length</p>
+            <img className="plus" src="/images/icon-plus.svg" />
+          </div>
 
-        <button className="cart-add-btn">
-        <img className="" src="/images/icon-cart.svg">
-        Add to cart
-        </button>
+          <button className="cart-add-btn">
+            <img className="" src="/images/icon-cart.svg"></img> Add to cart
+          </button>
         </div>
-
-        </div>
-    })
+      </div>
+    </div>
+  );
 }
 export default Product_Desc;
--- src/components/card/product_card.jsx
+++ src/components/card/product_card.jsx
@@ -1,14 +1,14 @@
-import Product_Desc './product_desc_card'
+import Product_Desc from './product_desc_card'
 import Product_Card_Gallery from "./product_card_gallery"
 
 function Product_Card({cart}){
-    return({
+    return(
         <div className="product-card">
         <Product_Card_Gallery />
         <Product_Desc
         cart={cart}
         />
         </div>
-    })
+    )
 }
 export default Product_Card;
